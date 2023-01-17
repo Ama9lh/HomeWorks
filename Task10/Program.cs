f5aff0fb0f17286ff2aac0d20b84d35a47e9c58f
Denis <kummerd65@gmail.com>
2023-01-17 19:00:26 +0600

    Task10 Complete

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿Console.Clear();
+while(true)
+{
+    Console.Write("Введите свое трёхзначное число: ");
+    int numb = Convert.ToInt32(Console.ReadLine());
+
+    if(numb >= 99 && numb <= 999)     // && - И,   || - Или,....
+    {
+        int temp = numb % 10;
+        temp = numb / 10;
+        temp = temp % 10; 
+        Console.WriteLine(temp);
+        break;
+    }
+
+    else
+    {
+        Console.WriteLine("Введено неверное число, введите трёхзначное число ");
+        
+
+    }
+}
+
+
+
+
+
