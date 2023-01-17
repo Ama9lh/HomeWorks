d1a3d24826753cd9307ced833b959641bfe31ede
Denis <kummerd65@gmail.com>
2023-01-17 15:29:11 +0600

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+if (k >= 100)
+{
+    while(k >= 100)
+    {
+        k /= 10;
+        int d = k % 10;
+        Console.WriteLine(d);    
+    }
+    
+}
+else 
+    Console.WriteLine ("Третьей цифры нет. ");
