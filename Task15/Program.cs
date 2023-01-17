efbdae28d3ca8171c46b927e440322cb081f992e
Denis <kummerd65@gmail.com>
2023-01-17 22:42:47 +0600

    add and complete Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int mon = 1;
+int tus = 2;
+int thu = 3;
+int fri = 4;
+int sat = 5;
+int sun = 6;
+int sum = 7;
+
+if (a == sun || a == sum)
+  Console.WriteLine("Да. ");
+
+else
+    Console.WriteLine("Нет. ");
+
+
+
+
+
