32a4f67f38089d6bd8ea14397545b17864e98be5
Denis <kummerd65@gmail.com>
2023-01-17 16:41:39 +0600

    complete Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -4,13 +4,13 @@ int k = Convert.ToInt32(Console.ReadLine());
 
 if (k >= 100)
 {
-    while(k >= 100)
+    while(k >= 999)
     {
         k /= 10;
-        int d = k % 10;
-        Console.WriteLine(d);    
     }
-    
-}
+
+int d = k % 10;
+    Console.WriteLine(d);
+}  
 else 
     Console.WriteLine ("Третьей цифры нет. ");
